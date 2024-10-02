a7a25bb4f4b2566751b94b09db5ef125aeca8e24
Gabriel <gabrieldias@ges.inatel.br>
2024-10-02 01:15:36 -0300

    mudando variavel de ambiente

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,3 +1,3 @@
 sudo apt-get install mailutils
-echo "A variável EMAIL_ADDRESS está definida como: $EMAIL_ADDRESS"
-echo "Atualização Código S107" | mail -s "prezado, boa noite! Apenas passando para lhe informar que existe uma nova versão do código de S107 do Vitor e Gabriel. Att, Chris" "$EMAIL_ADDRESS"
+echo "A variável EMAIL_ADDRESS está definida como: $EMAIL_ADDRESS_S107"
+echo "Atualização Código S107" | mail -s "prezado, boa noite! Apenas passando para lhe informar que existe uma nova versão do código de S107 do Vitor e Gabriel. Att, Chris" "$EMAIL_ADDRESS_S107"
