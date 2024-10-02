fcf64277db27981a8a952a80c58a543c45d20095
Gabriel <gabrieldias@ges.inatel.br>
2024-10-02 01:29:57 -0300

    mudando env

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,3 +1,3 @@
 sudo apt-get install mailutils
-echo "A variável EMAIL_ADDRESS está definida como: $super_secrets"
-echo "Atualização Código S107" | mail -s "prezado, boa noite! Apenas passando para lhe informar que existe uma nova versão do código de S107 do Vitor e Gabriel. Att, Chris" "$super_secrets"
+echo "A variável EMAIL_ADDRESS está definida como: $EMAIL_ADDRESS"
+echo "Atualização Código S107" | mail -s "prezado, boa noite! Apenas passando para lhe informar que existe uma nova versão do código de S107 do Vitor e Gabriel. Att, Chris" "$EMAIL_ADDRESS"
